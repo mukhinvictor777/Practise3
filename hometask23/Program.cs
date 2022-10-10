5972cc7ec8de9ed08b3470d2f61cee55b10e0df3
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-10 23:14:35 +0600

    homework done

--- hometask23/Program.cs
+++ hometask23/Program.cs
@@ -1,29 +1,29 @@
 ﻿int getNumberFromUser(string userInformation)
 {
     int  result = 0;
-    while (result == 0)
+    while (result == 0 || result < 1 || result > 1290)
     {
         Console.Write(userInformation);
         string userLine = Console.ReadLine();
         int.TryParse(userLine, out result);
-        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое число, вы вввели {userLine}"); else break;
+        if (result == 0 || result < 1 || result > 1290) Console.WriteLine($"Введите целое число от 1 до 1290, вы вввели {userLine}"); else break;
     }
     return result;
 }
 
-void tableOfSquares(int number)
+void tableOfCubes(int number)
 {
     Console.WriteLine();
-    Console.WriteLine($"Таблица квадратов чисел от 1 до {number}");
+    Console.WriteLine($"Таблица кубов чисел от 1 до {number}");
     Console.WriteLine();
     for (int i = 1; i <= number; i++)
     {   
-        int square = i*i;
-        Console.WriteLine($"Квадрат {i}^2 = {square}");               
+        int cube = i*i*i;
+        Console.WriteLine($"{i}^3 = {cube}");
     }
     Console.WriteLine();
 }
 
-Console.WriteLine("Введите целое число");
+Console.WriteLine("Введите целое число от 1 до 1290");
 int userNumber = getNumberFromUser("");
-tableOfSquares(userNumber);
+tableOfCubes(userNumber);
