1d84c32d4ebf12bc7525b9d3cb6b6310ad990179
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-10 21:29:40 +0600

    task22 done

--- task22/Program.cs
+++ task22/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int getCoordinateFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое число, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+void tableOfSquares(int number)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Таблица квадратов чисел от 1 до {number}");
+    Console.WriteLine();
+    for (int i = 1; i <= number; i++)
+    {   
+        int square = i*i;
+        Console.WriteLine($"Квадрат {i}^2 = {square}");               
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите целое число");
+int userNumber = getCoordinateFromUser("");
+tableOfSquares(userNumber);
