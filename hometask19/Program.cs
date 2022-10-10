66b790861957e7de9465b5b7ddfa8f4411f23441
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-10 21:38:35 +0600

    homework in process

--- hometask19/Program.cs
+++ hometask19/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое число, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+void tableOfSquares(int number)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Таблица квадратов чисел от 1 до {number}");
+    Console.WriteLine();
+    for (int i = 1; i <= number; i++)
+    {   
+        int square = i*i;
+        Console.WriteLine($"Квадрат {i}^2 = {square}");               
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите целое число");
+int userNumber = getNumberFromUser("");
+tableOfSquares(userNumber);
